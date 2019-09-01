d6d47235d90750826c74022d0e3c81a0100820a0
xiebing <xiebing@visionet.com.cn>
2019-09-01 23:59:36 +0800

    添加注释说明

--- src/main/scala/com/mouse/tool/IsLive.scala
+++ src/main/scala/com/mouse/tool/IsLive.scala
@@ -11,19 +11,23 @@ case class IsLive(ssc: StreamingContext) {
     * 优雅停止
     */
   def stopByMarkKey(): Unit = {
-    val intervalMills = 6 * 1000 // sparkStreaming每3秒拉去一次，检测代码每隔6秒扫描一次消息是否存在
+    val intervalMills = 6 * 1000
     var isStop = false
     while (!isStop) {
+      //等待执行停止。执行过程中发生的任何异常都会在此线程中抛出，如果执行停止了返回true，
+      //线程等待超时长，当超过timeout时间后，会监测ExecutorService是否已经关闭，若关闭则返回true，否则返回false。
       isStop = ssc.awaitTerminationOrTimeout(intervalMills)
       if (!isStop && isExists(stop_flag)) {
         Thread.sleep(2000)
+        //第一个true：停止相关的SparkContext。无论这个流媒体上下文是否已经启动，底层的SparkContext都将被停止。
+        //第二个true：则通过等待所有接收到的数据的处理完成，从而优雅地停止。
         ssc.stop(true, true)
       }
     }
   }
 
   /**
-    * 判断Key是否存在
+    * 判断Redis中的Key是否存在，存在返回为true
     */
   def isExists(key: String): Boolean = {
     val jedis = getConnet().getRedisConnet()
